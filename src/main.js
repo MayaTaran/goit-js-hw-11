1f96fbf26708872c56012f201df54ab5fc1362d9
MayaTaran <145223827+MayaTaran@users.noreply.github.com>
2024-03-01 20:32:53 +0200

    repository created

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const getForm = document.querySelector("form");
+console.log(getForm);
