af564ddeed81ecff8998ebfe4061ebc55cb75b48
MayaTaran <145223827+MayaTaran@users.noreply.github.com>
2024-03-03 17:06:04 +0200

    first version with import/export

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,8 +1,28 @@
-export function updateGallery(images) {
-    // ваша реалізація функції тут
-    getList.innerHTML = images.map(({ webformatURL, tags }) => {
-        return `<li> <img src="${webformatURL}" alt="${tags}"></li>`;
-    }).join('');
+export function createCard({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+  return `
+    <li class="card">
+      <a href="${largeImageURL}" data-caption="${tags}">
+        <img class="card-image" src="${webformatURL}" alt="${tags}" title="${tags}"/>
+        <ul class="discription-list">
+          <li>
+            <h3 class="disc-name">Likes</h3>
+            <p class="disc-value">${likes}</p>
+          </li>
+          <li>
+            <h3 class="disc-name">Views</h3>
+            <p class="disc-value">${views}</p>
+          </li>
+          <li>
+            <h3 class="disc-name">Comments</h3>
+            <p class="disc-value">${comments}</p>
+          </li>
+          <li>
+            <h3 class="disc-name">Downloads</h3>
+            <p class="disc-value">${downloads}</p>
+          </li>
+        </ul>
+      </a>
+    </li>`;
 }
 
 
